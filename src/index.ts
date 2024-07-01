785b68240a6817c4be3a2f259aec758e5cb82730
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-07-01 18:56:04 +0600

    added new migration to update schema

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,9 @@ async function insertUser(
   email: string,
   firstName: string,
   lastName: string,
-  password: string
+  password: string,
+  countryCode: string,
+  phoneNumber: string
 ) {
   const resp = await prisma.user.create({
     data: {
@@ -13,6 +15,8 @@ async function insertUser(
       firstName,
       lastName,
       password,
+      countryCode,
+      phoneNumber,
     },
   });
 
@@ -28,21 +32,32 @@ async function updateUser(
   email: string,
   { firstName, lastName }: UpdateParams
 ) {
-  const resp = await prisma.user.update({
-    where: {
-      email,
-    },
-    data: {
-      firstName,
-      lastName,
-    },
-  });
-
-  console.log(resp);
+  try {
+    const resp = await prisma.user.update({
+      where: {
+        email,
+      },
+      data: {
+        firstName,
+        lastName,
+      },
+    });
+    console.log(resp);
+  } catch (error: any) {
+    console.log("Got you bloody error");
+    console.log(error.message);
+  }
 }
 
-// insertUser("rahim2@karim.com", "Rahim", "Karim", "myPass");
-updateUser("rahim2@karim.com", {
-  firstName: "Rahim-updated-2",
-  lastName: "Karim",
-});
+insertUser(
+  "rahim234@karim.com",
+  "Rahim",
+  "Karim",
+  "myPass",
+  "+880",
+  "1752387156"
+);
+// updateUser("rahim2@karim.com", {
+//   firstName: "Rahim-updated-2",
+//   lastName: "Karim",
+// });
